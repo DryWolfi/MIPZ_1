44d7e00742d841d14f79cded7b3ef1c80a7c6a2f
dvovc <dvovc@DESKTOP-FUGVPPR>
2022-05-30 16:11:28 +0300

    Добавьте файлы проекта.

---
+++ MIPZ_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MIPZ_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
